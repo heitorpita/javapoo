77ae08a34a0178629f2c8b73b8dcc4459960e6a1
Heitor <heitor@tom.tom>
2025-04-23 20:26:41 -0300

    Primeiro commit Java Poo

---
+++ Main.java
@@ -0,0 +1,25 @@
+package Poo;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Pirata Luffy = new Pirata();
+        Luffy.nome = "Luffy";
+        Luffy.bando = "Chapeus De Palha";
+        Luffy.idade = 18;
+        Luffy.HakiAtivado();
+            //CRIAR PIRATA
+
+        Pirata Zoro = new Pirata();
+
+        Zoro.nome = "Zoro";
+        Zoro.bando = "Chapeus De Palha";
+        Zoro.idade = 18;
+        Zoro.HakiAtivado();
+
+
+    }
+
+
+}
---
+++ Pirata.java
@@ -0,0 +1,12 @@
+package Poo;
+public class Pirata {
+    String nome;
+    String bando;
+    int idade;
+//Criar um Metodo Publico Personalizado
+    public void  HakiAtivado(){
+        System.out.println("Haki Ativado");
+    }
+
+
+}
